13a7fedf93341cc39bc3fbf57ff1ed266fce838b
Paul Kelly <almond64@knights.ucf.edu>
2021-09-12 15:03:38 -0400

    Initial commit failed

---
+++ src/main/java/Solution09.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+        int width = input.nextInt();
+        double squareFeet = length*width;
+        double num_of_gallons = squareFeet / 350; //find out how to round up to the nearest whole number
+        System.out.printf("You will need to purchase %d gallons of paint to cover %d square feet.\n", Math.round(num_of_gallons), (int)squareFeet);
+    }
+}
